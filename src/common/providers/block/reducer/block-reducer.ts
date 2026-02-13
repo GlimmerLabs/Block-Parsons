5f4ecf15ce18ae5709e10bfdcd32579f53d599e0
Julian Kim <kimjulian@grinnell.edu>
2026-02-13 16:50:37 -0600

    Implement block to Scamper code conversion

--- src/common/providers/block/reducer/block-reducer.ts
+++ src/common/providers/block/reducer/block-reducer.ts
@@ -8,6 +8,12 @@ import {
   solutionBlocks,
 } from "../../../../problem-gen/initial-state.ts";
 import { isEqual } from "es-toolkit";
+import {
+  type BlockData,
+  isBlockWithChildrenData,
+  isConstantBlockData,
+} from "../../../block-types.ts";
+import { throwNull } from "../../../utils.ts";
 
 export type BlockDispatchType =
   | {
@@ -25,6 +31,75 @@ export type BlockDispatchType =
       type: "CHECK";
     };
 
+interface ConversionError {
+  type: "ConversionError";
+  message: string;
+}
+interface ConversionSuccess {
+  type: "ConversionSuccess";
+  code: string;
+  blocksEncountered: number;
+}
+type ConversionResult = ConversionSuccess | ConversionError;
+
+function isConversionError(
+  result: ConversionResult,
+): result is ConversionError {
+  return result.type === "ConversionError";
+}
+function convertBlocksToScamper(
+  blocks: BlockContextType["blocks"],
+  topLevel: BlockContextType["solutionTopLevel"],
+): ConversionResult {
+  const count = blocks.size;
+
+  function scamperifyBlock(block: BlockData): ConversionResult {
+    if (isConstantBlockData(block)) {
+      return {
+        type: "ConversionSuccess",
+        code: block.value,
+        blocksEncountered: 1,
+      };
+    }
+    if (!isBlockWithChildrenData(block))
+      return throwNull("block is neither constant nor block with children?");
+    // else is block with children
+    const childCode: string[] = [];
+    let blocksEncountered = 1;
+    for (const { id } of block.children) {
+      if (!id) return { type: "ConversionError", message: "null child id" };
+      const conversionResult = scamperifyBlock(
+        blocks.get(id) ?? throwNull("child block id is not a real block?"),
+      );
+      if (isConversionError(conversionResult)) return conversionResult;
+      childCode.push(conversionResult.code);
+      blocksEncountered += conversionResult.blocksEncountered;
+    }
+    return {
+      type: "ConversionSuccess",
+      code: `(${childCode.join(" ")})`,
+      blocksEncountered,
+    };
+  }
+
+  let finalCode: string = "";
+  let blocksEncountered = 0;
+  for (const blockId of topLevel) {
+    const block =
+      blocks.get(blockId) ?? throwNull("top level block not found?");
+    const conversionResult = scamperifyBlock(block);
+    if (isConversionError(conversionResult)) return conversionResult;
+    finalCode += conversionResult.code;
+    blocksEncountered += conversionResult.blocksEncountered;
+  }
+  return blocksEncountered === count
+    ? { type: "ConversionSuccess", code: finalCode, blocksEncountered }
+    : {
+        type: "ConversionError",
+        message: `blocks encountered don't match count (${blocksEncountered.toString()} / ${count.toString()})`,
+      };
+}
+
 export const blockReducer: ImmerReducer<BlockContextType, BlockDispatchType> = (
   draft: Draft<BlockContextType>,
   action: BlockDispatchType,
@@ -40,6 +115,13 @@ export const blockReducer: ImmerReducer<BlockContextType, BlockDispatchType> = (
     }
     case "CHECK": {
       // TODO: make not naive solution checking (probably relies on Scamper integration)
+      console.log(
+        "conversion result: ",
+        convertBlocksToScamper(
+          current(draft).blocks,
+          current(draft).solutionTopLevel,
+        ),
+      );
       return {
         ...draft,
         solutionIsCorrect: isEqual(current(draft).blocks, solutionBlocks),
